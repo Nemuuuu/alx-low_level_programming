f2b3c3dd326a7e7f804027823feba64d647d41bb
Neima Nesru <neimanesru34@gmail.com>
2022-09-09 02:33:04 -0700

    commited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,6 +8,7 @@ int main(void)
 	int first_nums; /*Decarling statement*/
 	int second_nums;
 	/*for 0-9 with ASCII*/
+
 	for (first_nums = 0; first_nums < 100 ; first_nums++)
 	{
 		for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
