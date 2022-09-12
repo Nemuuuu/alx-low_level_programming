0c225ba41cd1507bd1641f37e732271f9253c6c4
Neima Nesru <neimanesru34@gmail.com>
2022-09-12 06:48:00 -0700

    commited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 /**
+ *
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-int left;/*declaration*/
-int right;
+   int main(void)
+ {
+      int left;/*declaration*/
+      int right;
 
-for(left=0;left<9;left++)
-{
-	for (right=left+1;right<=9;right++)
-	{
-		putchar('0'+left); /*print  number*/   
-		putchar('0'+right);
+      for(left=0;left<9;left++)
+      {
+        	for (right=left+1;right<=9;right++)
+	        {
+		          putchar('0'+left); /*print  number*/   
+	         	  putchar('0'+right);
 
-		if(left<8)
-		{
-			putchar(','); /*print (,)*/   
-			putchar(' '); /*print a space*/   
-		}
-	}
-}
-putchar('\n');/*new line*/ 
+	                  if(left<8)
+		          {
+			       putchar(','); /*print (,)*/   
+			       putchar(' '); /*print a space*/   
+	                  }
+	        }
+     }
+      putchar('\n');/*new line*/ 
 
-return (0);
-}
+      return (0);
+ }
 
