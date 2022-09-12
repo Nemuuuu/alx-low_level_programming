69296bd2ecb9bda9ca812f779cdcae4cbf8baf8a
Neima Nesru <neimanesru34@gmail.com>
2022-09-12 07:35:03 -0700

    commited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,19 +3,18 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
- int main(void)
- {
-   int left; /*declaration*/
-   int right;
+int main(void)
+{
+int left; /*declaration*/
+int right;
 
-      for (left = 0; left < 9; left++)
-      {
-        	for (right = left + 1; right <= 9; right++ )
-	        {
-		          putchar('0' + left ) ; /*print  number*/   
-	         	  putchar('0' + right ) ;
-
-	                  if (left < 8)
+for (left = 0; left < 9; left++)
+{
+	for (right = left + 1; right <= 9; right++)
+	{
+		putchar('0' + left); /*print  number*/   
+		putchar('0' + right);
+			if (left < 8)
 		          {
 			       putchar(','); /*print (,)*/   
 			       putchar(' '); /*print a space*/   
