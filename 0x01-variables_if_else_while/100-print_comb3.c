0ba4755146460b29e6614aa5c8a9fe878573f503
Neima Nesru <neimanesru34@gmail.com>
2022-09-12 06:13:35 -0700

    commited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,25 +5,21 @@
  */
 int main(void)
 {
-	int number_left; /*Decarling statement*/
-	int number_right;
-	/*for 0-9 with ASCII*/
-
-	for (number_left = 01; number_left <= 09; number_left++)
-	{
-		for (number_right = 12; number_right <= 19; number_right++)
-		{
-			putchar(number_left); /*print  number*/
-			putchar (number_right);
-			/*break program if the condition is true (if is 89)*/
-			if ((number_left == 16) && (number_right == 29))
-			{
-				break; /*terminate the program*/
-			}
-			putchar(','); /*print (,)*/
-			putchar (' '); /*print a space*/
-		}
-	}
-	putchar('\n'); /*new line*/
-	return (0);
+int left;
+int right;
+for(left=0;left<9;left++)
+{
+for(right=left+1;left<=9;left++)
+{
+putchar('0'+left);
+putchar('0'+right);
+if(left<8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
