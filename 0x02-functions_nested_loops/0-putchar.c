d363ba154b66c8bba2735bf61d242494acc4f902
Neima Nesru <neimanesru34@gmail.com>
2022-09-13 02:46:52 -0700

    commited

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+int main(void)
+{
+	int i; /*Declaring statements*/
+	char string[9] = "_putchar"; /*Arrays*/
+	for (i = 0 ; i < 9 ; i++) /*Start for*/
+	{
+		_putchar(string[i]);/*display word*/
+	} /*End for*/
+	_putchar('\n');
+	return (0);
+}
