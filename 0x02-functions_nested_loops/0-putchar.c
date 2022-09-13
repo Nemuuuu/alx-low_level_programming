8acbdc2c1d59c2e97b3a5daae860c6796888fe06
Neima Nesru <neimanesru34@gmail.com>
2022-09-13 02:48:47 -0700

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	int i; /*Declaring statements*/
+
 	char string[9] = "_putchar"; /*Arrays*/
-	for (i = 0 ; i < 9 ; i++) /*Start for*/
+	for (i = 0 ; i < 8 ; i++) /*Start for*/
 	{
 		_putchar(string[i]);/*display word*/
 	} /*End for*/
