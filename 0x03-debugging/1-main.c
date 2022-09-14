273c5cc02447ca4d00b6c0334d439599ecb201e1
Neima Nesru <neimanesru34@gmail.com>
2022-09-14 05:11:17 -0700

    commited

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,12 @@
 
 	printf("Infinite loop incoming :(\n");
 
-	/*i = 0;
-	 *while (i < 10)
-	 *{
-	 *Infinite loop - No increase of variable
-	 *putchar(i);
-	 *}
+	/* i = 0;
+	 * 
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 
