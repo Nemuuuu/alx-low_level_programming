5483fbd83d5f0e7553674d11bea3c86927f97f05
Neima Nesru <neimanesru34@gmail.com>
2022-09-14 23:46:34 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long n, i;
+
+	n = 612852475143;
+	for (i =2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = n / ;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
