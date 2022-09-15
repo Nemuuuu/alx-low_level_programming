e98b9dd834f02162ec960651a266b6dfd40cb021
Neima Nesru <neimanesru34@gmail.com>
2022-09-14 23:29:32 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int n = 100;
+	int i;
+
+	i = 1;
+	while (i <= n )
+	{
+		if (i % 3 == 0 && i % 5== 0) /* multiply of 3 and 5*/
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i < n)
+				printf("Buzz ");
+				else
+					printf("Buzz");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
+
