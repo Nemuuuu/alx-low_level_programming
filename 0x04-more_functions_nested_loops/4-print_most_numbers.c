d0efb32cc4bd9438293a5d0b90e52d77bcbd1420
Neima Nesru <neimanesru34@gmail.com>
2022-09-14 22:40:01 -0700

    commited

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 	int i; /*Declaring statement*/
 
 	i = 0;
-	while (i<10); /*Start While*/
+	while (i < 10); /*Start While*/
 	{
 		if (i != 2 && i != 4) /*No print 2 and 4*/
 		{
@@ -19,4 +19,4 @@ void print_most_numbers(void)
 		i++;
 	}
 	_putchar ('\n'); /*New line*/
-}
+i}
