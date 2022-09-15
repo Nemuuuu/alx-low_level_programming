25fd5f5453ed8a7225d63e8d6a266c38febd8b4b
Neima Nesru <neimanesru34@gmail.com>
2022-09-15 03:47:42 -0700

    commited

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,9 @@ void print_most_numbers(void)
 	{
 		if (i != 2 && i != 4) /*No print 2 and 4*/
 		{
-			_putchar(i +'0'); /*print value i*/
+			_putchar(i + '0'); /*print value i*/
 		}
 		i++;
 	}
-	
 	_putchar('\n'); /*New line*/
 }
