a7909975c6afae4033577649caeda63c0aef5f31
Neima Nesru <neimanesru34@gmail.com>
2022-09-14 23:20:23 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: print size
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int a;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (a = 0; a < size; a++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
