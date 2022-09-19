7b4daf30898401cf9fd1b6a9d2a05c9fd5dd212b
Neima Nesru <neimanesru@gmail.com>
2022-09-18 23:30:36 -0700

    puts2 file is updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,9 @@ void puts2(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0);
-		_putchar(str[i]);/*diplay contents*/
+		{
+			_putchar(str[i]);/*diplay contents*/
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
