52dc01496bde5fdbc567900f05e1100e3fc16745
Neima Nesru <neimanesru@gmail.com>
2022-09-18 22:34:46 -0700

    strlen added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: a pointer to an int that will be updated
+ *
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i] != '0') /*count character of string*/
+	{
+		i++;
+	}
+	return (i);
+}
+			   
+
