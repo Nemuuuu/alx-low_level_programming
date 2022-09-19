eca9aab7e8cce74712bd580cb447cf228a5a6d19
Neima Nesru <neimanesru@gmail.com>
2022-09-18 22:47:23 -0700

    file puts is added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string 
+ * @str: a pointer to an int that will be updated
+ *
+ * Retur: void
+ */
+void _puts(char *str)
+{
+	char *c;
+	int m;
+
+	c = str;
+
+	for (m = 0; c[m]; m++)
+	{
+		_putchar(c[m]);
+	}
+	_putchar('\n');
+}
