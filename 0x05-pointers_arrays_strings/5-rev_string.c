308dcca9074d3ee75482c6b420cc1c4b41f07908
Neima Nesru <neimanesru@gmail.com>
2022-09-18 23:20:36 -0700

    rev string fileis added

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,25 +10,25 @@ void rev_string(char *s)
 {
 	char *start_c, *end_c, c;
 	int i, count;
-	int length;
+	int length = 0;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i]; i++) /*count character by character*/
 	{
 		length++;
 	}
-	count = length;
-	start_c = s;
+	count = length;/*count each character of string*/
+	start_c = s;/*start_c and end_c are values of equal variables*/
 	end_c = s;
-	for (i = 0; i < count - 1; i++)
+	for (i = 0; i < count - 1; i++)/*move the end_c to last character*/
 	{
 		end_c++;
 	}
-	for (i = 0; i < count / 2; i++)
+	for (i = 0; i < count / 2; i++)/*swap char from start and end*/
 	{
-		c = *end_c;
+		c = *end_c;/*swap character*/
 		*end_c = *start_c;
 		*start_c = c;
-		start_c++;
+		start_c++;/*update pointer positions*/
 		end_c++;
 	}
 }
