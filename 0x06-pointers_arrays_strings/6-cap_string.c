4a065ebe680e8f0f8b9a601fa68f21e52b488fce
Neima Nesru <neimanesru@gmail.com>
2022-09-21 00:16:21 -0700

    capitalized

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * separator - checks if character is a separator
+ * @c: character to be checked
+ *
+ * Return: if separator return 1. otherwise return 0
+ */
+int separator(char c)
+{
+	switch (c)
+	{
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			return (1);
+		default: 
+			return (0);
+	}
+}
+/**
+ * cap_string - capitalize chars after given deliminators
+ * @s: string to uppercase
+ *
+ * Return: returns modified string
+ */
+char *cap_string(char *s)
+{
+	int count, upper;
+
+	upper =-32;
+	count = 0;
+	while (s[count] != '\0')
+	{
+		if (s[count] >= 'a' && s[count] <= 'z')
+		{
+			if (s[count] == *s || separator(s[count - 1]))
+					{
+					s[count] += upper;
+					}
+					}
+					count++;
+					}
+					return (s);
+					}
+					
+
