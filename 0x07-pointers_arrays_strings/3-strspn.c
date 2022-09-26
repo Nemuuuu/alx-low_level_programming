aeb5a5742ccc719c7396c16ab024c3d2af999f91
Neima Nesru <neimanesru@gmail.com>
2022-09-25 21:57:38 -0700

    strspn added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - locates a character in a string
+ * @s: this is the main c string to ne scanned
+ * @accept: this is the string containing the list of characters to match in s
+ * Return: count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+	i = 0;
+	while (str1[i] != '\0')
+	{
+		j = 0;
+		while (str2[j] != '\0')
+		{
+			if (str2[j] == str1[i])
+			{
+				count++;
+				break;
+			}
+			j++;
+		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+		i++;
+	}
+	return(0);
+}
