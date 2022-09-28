bea3dc39714edd33b7c074d8cea8dccb8bfe2705
Neima Nesru <neimanesru@gmail.com>
2022-09-27 22:24:27 -0700

    prime number added

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * evaluate_num - recursion loop
+ * @num: number
+ * @iterator: number to iterate
+ *
+ * Return: return 1 to 0
+ */
+int evaluate_numb(int num, int iterator)
+{
+	if (iterator == num - 1)
+	{
+		return (1);
+	}
+	else if (num % iterator == 0)
+	{
+		return (0);
+	}
+	if (num % iterator != 0)
+	{
+		return (evaluate_num(num, iterator +));
+	}
+	return (0);
+}
+
+/**
+ * is_prime_number - evaluate prime number
+ * @num: number
+ *
+ * Return: return 1 if prime, return 0 otherwise
+ */
+int is_prime_number(int num)
+{
+	int iterator;
+
+	iterator = 2;
+	if (num < 2)
+	{
+		return (0);
+	}
+	if (num == 2)
+	{
+		return (1);
+	}
+	return (evaluate_num(num, iterator));
+}
