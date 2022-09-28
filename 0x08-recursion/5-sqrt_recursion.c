8755859ba75689a9eb5df6fd76ebe5962f850083
Neima Nesru <neimanesru@gmail.com>
2022-09-27 22:10:50 -0700

    square root of a number added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _evaluate - evaluate function square root
+ * @i: integer
+ * @n: integer
+ *
+ * Return: evaluate sqrt
+ */
+int _evaluate(int i, int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else if (i * i < n)
+	{
+		return (_evaluate(i +1, n));
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer 
+ *
+ * Return: square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+		return (_evaluate(i, n));
+}
+
