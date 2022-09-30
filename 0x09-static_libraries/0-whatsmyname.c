bf3ac6ab10003f01c6c9befce83f1f585b06be93
Neima Nesru <neimanesru@gmail.com>
2022-09-30 05:06:47 -0700

    prints name

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: counts the argument
+ * @argv: arguments
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[]);
+	return (0);
+}
