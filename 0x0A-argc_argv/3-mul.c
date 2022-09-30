9e810a0d17b7b57e9032d1605c76d2d143d897b6
Neima Nesru <neimanesru@gmail.com>
2022-09-30 06:21:34 -0700

    numbers are added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,27 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int count;
-	int num1;
-	int num2;
-	int result;
+	int num1, num2;
 
-	(void) argc;
-	count = 1;
-	if (argv[count] != '\0')
+	if (argc == 3)
 	{
-		if (argv[count] == NULL || argv[count + 1] == NULL)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			num1 = atoi(argv[count]);
-			num2 = atoi(argv[count + 1]);
-			result = num1 * num2;
-			printf("%d\n", result);
-		}
+		num1 = atoi(argv[1]);
+		num1 = atoi(argv[2]);
+		printf("%d\n", (num1 * num2));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
