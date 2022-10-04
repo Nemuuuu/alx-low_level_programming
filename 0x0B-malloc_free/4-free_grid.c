1eec14d3cfc40a5755099e0e3b680bd3fa227b70
Neima Nesru <neimanesru@gmail.com>
2022-10-03 23:09:36 -0700

    grid freed

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2D grid previously created by alloc_grid function
+ * @grid: pointer contains width address
+ * @height: column of array
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < heightl i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
