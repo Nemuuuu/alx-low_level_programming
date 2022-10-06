041144a6b06821690db7d732a7b6981072a1ca02
Neima Nesru <neimanesru@gmail.com>
2022-10-05 21:57:45 -0700

    memory allocated

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: positive integer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
