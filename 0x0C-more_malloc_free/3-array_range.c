206d4bf88511f9480d6a86bf1650b7faaab58e44
Neima Nesru <neimanesru@gmail.com>
2022-10-05 22:50:35 -0700

    array is creared

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: number of memory bytes
+ * @max: number of data type bits
+ *
+ * Return: ptr to initialized buffer
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	unsigned int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; min <= max; i++)
+	{
+		p[i] = min;
+		min++:
+	}
+	return (p);
+}
