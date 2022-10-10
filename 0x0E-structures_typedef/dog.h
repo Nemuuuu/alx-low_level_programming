33980cd7ccf010db03d158093af601977125e610
Neima Nesru <neimanesru@gmail.com>
2022-10-09 22:36:58 -0700

    structure definition added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure called dog
+ * @name: name of dog
+ * @age: age of dog
+ *@owner: owner of dog
+ *
+ * This structure defines 3 members name as char type, age as float type and owner as char type
+ */
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
