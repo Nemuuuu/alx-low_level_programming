8df1f7d29ece891a38e60ca0376f9b710620a688
Neima Nesru <neimanesru@gmail.com>
2022-10-10 04:22:00 -0700

    file name printed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 
 /**
@@ -6,20 +9,22 @@
  * @age: age of dog
  *@owner: owner of dog
  *
- * This structure defines 3 members name as char type, age as float type and owner as char type
+ * This structure defines 3 members name as char type,
+ * age as float type and owner as char type
  */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
-}dog_t;
+	char *owner;
+} dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+#endif /*DOG_H*/
