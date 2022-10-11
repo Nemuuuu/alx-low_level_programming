c2b55f783270890bca3bb72e0f01f33f416b640c
Neima Nesru <neimanesru@gmail.com>
2022-10-11 02:44:50 -0700

    structure initialised

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,15 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
+ * Description: this is struct for dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
