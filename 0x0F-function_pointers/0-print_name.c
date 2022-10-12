5c1e8c2db56eea19dabbd9cd06b96b9b54db9206
Neima Nesru <neimanesru@gmail.com>
2022-10-11 21:21:54 -0700

    name printed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * printf_name - function that prints name
+ * @name: name
+ * @f: callback function
+ *
+ * Description: function that prints a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name !=NULL && f != NULL)
+		f(name);
+}
