598e110eb56d76e091da6f539f86cf59c8f2d482
Neima Nesru <neimanesru@gmail.com>
2022-10-22 03:13:10 -0700

    every minutes of jack bauer is printed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 60; i++)
+	for (j = 0; j < 60; j++)
 	{
-		for ( j = 0; j < 24; j++)
+		for ( i = 0; i < 24; i++)
 		{
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
