362115ba88cc121d79cec60a6025b81e4e76604a
Neima Nesru <neimanesru@gmail.com>
2022-10-22 00:30:29 -0700

    alphabets printed 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,18 @@
 /**
  * print_alphabet - prints alphabet in lower case
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	int i = 97;
 	int j;
 
 	for (j = 0; j < 10; j++)
 	{
+		int i = 97;
 		while (i <= 122)
-	{
-		_putchar(i);
-		i++;
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
