29bd5fd529c4e9462931c60948a4b8a24d1bfa84
Neima Nesru <neimanesru@gmail.com>
2022-10-22 04:12:08 -0700

    9 times table is printed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,25 @@
 void times_table(void)
 {
 	int i, j;
+	int time1, time2;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i * j) + '0');
+			if ((i * j) > 9)
+			{
+				time1 = (i * j) / 10;
+				time2 = (i * j) % 10;
+				_putchar(time1 + '0');
+				_putchar(time2 + '0');
+			}
+			else
+				_putchar((i * j) + '0');
+			if (j == 9)
+			{
+				break;
+			}
 			_putchar(',');
 			_putchar(' ');
 		}
