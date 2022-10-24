3dd6a3ca92e505cef63d5198788bbeeab8e7c1bc
Neima Nesru <neimanesru@gmail.com>
2022-10-23 22:58:56 -0700

    diagonal printed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,11 +19,3 @@ void print_diagonal(int n)
         putchar('\n');
        }
 }
-int main(void)
-{
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
-}
