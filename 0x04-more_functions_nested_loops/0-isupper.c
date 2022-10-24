f9106731bc37fe33c943dd4e233c061f0319cffc
Neima Nesru <neimanesru@gmail.com>
2022-10-23 22:51:05 -0700

    letter is checked if it is uppercase or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,13 +11,3 @@ int _isupper(int c)
 {
     return (c >= 65 && c <= 90);
 }
-int main(void)
-{
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
-}
