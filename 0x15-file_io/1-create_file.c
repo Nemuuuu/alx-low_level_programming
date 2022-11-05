94712a3c4e6ff6712d924346bf8643a9f04ecb93
Neima Nesru <neimanesru@gmail.com>
2022-11-04 22:31:53 -0700

    file created

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,13 +3,14 @@
 /**
  * create_file - create a file
  * @filename: name of file to create
- * @text_contect: string to write to the file
+ * @text_content: string to write to the file
  *
  * Return: 1 on success, otherwise -1
  */
 int create_file(const char *filename, char *text_content)
 {
 	int x, write_file;
+
 	if (filename == NULL)
 		return (-1);
 	x = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
