fb7e22545d3d31c11dff7b2dc9ece0e21c6465e5
Neima Nesru <neimanesru@gmail.com>
2022-12-12 09:13:02 -0800

    scripte added

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-gcc -Wall -fPIC -c *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -g -fPIC -Wall -Werro -Wextra -pedantic *.c -shared -o liball.so
