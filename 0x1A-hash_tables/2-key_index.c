ac813828e202b3096e404f2d02281a18f9290bbe
Nemuuuu <111045567+Nemuuuu@users.noreply.github.com>
2023-01-06 19:44:14 +0100

    update

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,12 +9,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long hash = 0;
-    int i;
-
-    for (i = 0; key[i]; i++)
-    {
-        hash += key[i];
-    }
-    return hash % size;
+   return (hash_djb2(key) % size);
 }
